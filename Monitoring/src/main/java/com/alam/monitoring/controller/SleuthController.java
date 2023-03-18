a9c642d1cef3ccf245d23c02f6c4ed5a3281fd5a
ALAM I, PARVEZ (ADV D AA PAMC NM DH1) <parvez.alam-i@siemens.com>
2023-03-19 02:05:34 +0530

    Added 'Scheduled_Support' Branch

--- Monitoring/src/main/java/com/alam/monitoring/controller/SleuthController.java
+++ Monitoring/src/main/java/com/alam/monitoring/controller/SleuthController.java
@@ -2,7 +2,6 @@ package com.alam.monitoring.controller;
 
 import com.alam.monitoring.service.SleuthService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +13,6 @@ public class SleuthController {
 
     private final SleuthService sleuthService;
 
-    @Autowired
     private Executor executor;
 
     public SleuthController(SleuthService sleuthService) {
